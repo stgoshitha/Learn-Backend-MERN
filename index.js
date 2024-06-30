ed06224e3b9a65d3e9ca738de52eeec3817b2074
Goshitha Premachandra <wkgoshithapremachandra@gmail.com>
2024-06-30 10:03:25 +0530

    implement bodyParser in project

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser') 
+
 const app = express()
 const port = 3000
+
+// This line configures the app to parse incoming requests with application/x-www-form-urlencoded payloads. This is typically used for form submissions.
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//This line configures the app to parse incoming requests with JSON payloads. This is commonly used in APIs where the client sends data in JSON format.
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World! - Get method')
